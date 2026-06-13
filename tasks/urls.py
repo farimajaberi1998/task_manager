1a37c8738a66f071c0a9cbec4cbbf004ee473614
farimajaberi1998 <farimajaberii@gmail.com>
2026-06-13 13:46:21 +0330

    refactor(api): implement full CRUD functionality using DRF ModelViewSet and DefaultRouter

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import TaskListCreateView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TaskViewSet
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
-    path('tasks/', TaskListCreateView.as_view(), name='task-list'),
+    path('', include(router.urls)),
 ]
